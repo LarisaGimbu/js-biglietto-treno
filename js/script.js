a3b7764c316a9f8c18a6ffd62fdea49a5fd8c1ef
LarisaGimbu <gimbularisa@gmail.com>
2021-10-21 18:24:00 +0200

    controllo-validità-dati

--- js/script.js
+++ js/script.js
@@ -4,24 +4,62 @@
 //4 sconto SE (età < 18 allora prezzo tot - 20%) altrimenti SE (età > 65 allora prezzo tot - 40%) altrimenti (prezzo totale)
 //
 
-const km = prompt("Inserire il numero di km che desidera percorrere:"); 
-const age = prompt("Inserire la vostra età:"); 
+const km = 50; 
+const age = 15; 
 
 const ticketPrice = km * 0.21 ;
 // console.log(ticketPrice);
 
+let bigliettoValido = true;
+let errorMsg = '';
+
+if(isNaN(km)){
+  bigliettoValido = false;
+  errorMsg = 'Inserire un numero di kilometri valido';
+}
+if(isNaN(age)){
+  bigliettoValido = false;
+  errorMsg = 'Inserire un\'età valida';
+}
+if(age <0 || age > 150){
+  bigliettoValido = false;
+  errorMsg = 'Inserire un\'età valida';
+}
+console.log(km);
+console.log(age);
+console.log(bigliettoValido);
+console.log(errorMsg);
+
 let totalPrice = ticketPrice;
+let outputText = `
+  Avete scelto un percorso lungo ${km} km,<br>
+  si conseguenza il vostro biglietto avrà un costo di <strong>${ticketPrice} euro.</strong>`;
+
 if( age < 18 ){
   totalPrice = ticketPrice - (ticketPrice * 0.20);
+  outputText = `
+  Avete scelto un percorso lungo ${km} km, <br>
+  di conseguenza il vostro biglietto ha un costo di ${ticketPrice} euro.<br>
+  Considerando però la vostra età di ${age} anni, <br>
+  vi concediamo uno sconto del 20% per un totale di : <strong>${totalPrice} euro</strong>`;
 }else if( age > 65){
   totalPrice = ticketPrice - (ticketPrice * 0.40);
+  outputText = `
+  Avete scelto un percorso lungo ${km} km,<br>
+  di conseguenza il vostro biglietto avrà un costo di ${ticketPrice} euro.<br>
+  Considerando però la vostra età di  ${age} anni, <br> 
+  vi concediamo uno sconto del 40% per un totale di : <strong>${totalPrice} euro </strong>`;
 }
 
 // console.log(totalPrice);
 
-document.getElementById("output").innerHTML = `
-  Il vostro biglietto costa: ${totalPrice.toFixed(2)} euro
-`
+
+if(!bigliettoValido){
+  outputText = `Errore!  ${errorMsg};`
+}
+
+
+document.getElementById("output").innerHTML = outputText;
 
 
 
