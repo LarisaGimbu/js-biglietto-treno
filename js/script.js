1380e856c0ac6c5543a36e0cc072c49dff16c316
LarisaGimbu <gimbularisa@gmail.com>
2021-10-21 14:27:10 +0200

    prezzo-al-km

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//1 variabie km che l'utente vuole percorrere
+//2 variabile età dell'utente
+//3 variabile prezzo totale ( km * 0.21)
+//4 sconto SE (età < 18 allora prezzo tot - 20%) altrimenti SE (età > 65 allora prezzo tot - 40%) altrimenti (prezzo totale)
+//
+
+const km = 20; //da cambiare con prompt
+const age = 20; //da cambiare con prompt
+
+const ticketPrice = km * 0.21 ;
+// console.log(ticketPrice);
+
+
+
+
